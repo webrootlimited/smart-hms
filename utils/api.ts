ee7a127974db1be45d9566d105007dd7a3942346
webrootlimited <info@webrootlimited.co.uk>
2026-04-01 18:44:43 +0500

    backend url added

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,7 @@
 const isDev = process.env.NODE_ENV === "development";
 const config = {
-  baseURL: "http://localhost:5000",
+  baseURL: isDev
+    ? "http://localhost:5000"
+    : "https://smart-hms-backend-production.up.railway.app",
 };
 export default config;
